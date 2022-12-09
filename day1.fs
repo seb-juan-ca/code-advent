39e6245265fde7edbde008099e0bf85b2f489eb4
Juan C @seb <seb-juan-ca>
2022-12-09 20:52:16 +0100

    day1 part b

--- day1.fs
+++ day1.fs
@@ -1,20 +1,22 @@
 open System.IO
 module caloriesParser=
-    let getCurrentMax (max:int, groupMax:int) (el:string) =
+    let getCurrentMax (maxes:seq<int>, groupMax:int) (el:string) =
         let isEmpty = el.Length=0
-        let currentMax=if max<groupMax then groupMax else max
         match isEmpty with
-        | true -> (currentMax, 0)
-        | false -> (max, groupMax + (int el))
+        | true -> (Seq.append maxes [groupMax], 0)
+        | false -> (maxes, groupMax + (int el))
 
     let getMax sequence=
-        let (accMax,lastGroupMax)=Seq.fold getCurrentMax (0,0) sequence
-        if accMax<lastGroupMax then
-            lastGroupMax
-        else
-            accMax
+        let (accMax,lastGroupMax)=Seq.fold getCurrentMax ([],0) sequence
+        accMax
+            |> Seq.sortDescending
+            |> Seq.distinct
+            |> Seq.truncate 3
     
     let getMaxFromFile filename=
         File.ReadLines filename |> getMax
+
+    let getMax3FromFile filename
+        File.ReadLines filename |> getMax |> Seq.sum;
 
 
