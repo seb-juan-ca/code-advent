1bc5d5bdb56c4ceefbb782777ed20aeb78ac327d
Juan C @seb <seb-juan-ca>
2022-12-09 20:52:40 +0100

    move files

---
+++ Coding2022/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let sumX (rows:seq<string>) =
+    rows |>
+        Seq.fold
+            (fun acc v -> v+"hola")
+            "--"
+let z=sumX ["a"]
+printfn "%A" z
